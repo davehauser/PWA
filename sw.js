e4b72435a9c34f35e558f0a7f232d9542a371ec0
Dave Hauser <dh@opo.li>
2018-04-09 21:33:51 +0200

    Use sw-toolbox to register service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,18 @@
 self.addEventListener('fetch', function(event) {
+  
+  // intercept image requests
   if(/\.jpg$/.test(event.request.url)) {
     event.respondWith(fetch('images/unicorn.jpg'));
   }
+
+  // cache page
+  toolbox.router.get('caching-page.html', function(event) {
+    return caches.open('cachingpage').then(function(response) {
+      return response.match(event.request).then(function(response) {
+        return response || fetch(event.request);
+      });
+    }).catch(function() {
+      return fetch(event.request);
+    });
+  });
 });
